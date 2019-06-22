4606eca4eb7929abec20497929bb986ebc56da98
Poe <piotr.a.ptak@icloud.com>
2019-06-22 22:23:23 +0200

    Add AppHeader prototype

--- src/pages/GameSelectionPage.js
+++ src/pages/GameSelectionPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GamesContainer from '../containers/GamesContainer';
 import Layout from '../components/Layout';
+import AppHeader from '../components/AppHeader';
 
 export default function GameSelectionPage() {
   return (
     <Layout columned>
+      <AppHeader />
       <GamesContainer />
     </Layout>
   );
