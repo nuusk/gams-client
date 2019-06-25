be970df9983f9a078787807db687ec6eecdb90bb
Poe <piotr.a.ptak@icloud.com>
2019-06-26 00:14:42 +0200

    Add react router and create login page prototype

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import { Switch } from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store/store';
 import theme from './styles/theme';
 import GlobalStyle from './styles/GlobalStyles';
 import GameSelectionPage from './pages/GameSelectionPage';
+import LoginPage from './pages/LoginPage';
 
 render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
       <>
-        <GameSelectionPage basename={process.env.PUBLIC_URL} />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={GameSelectionPage} />
+            <Route exact path="/login" component={LoginPage} />
+          </Switch>
+        </BrowserRouter>
         <GlobalStyle />
       </>
     </ThemeProvider>
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import AppLogo from '../components/AppLogo';
+
+export default function LoginPage() {
+  return (
+    <Layout columned narrow>
+      <header>
+        <AppLogo />
+      </header>
+      <div>logowanko</div>
+      <footer>
+        2019
+      </footer>
+    </Layout>
+  );
+}
