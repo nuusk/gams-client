c6177871d8416debfb4ae602a87cd94500540751
Poe <piotr.a.ptak@icloud.com>
2019-07-23 23:25:44 +0200

    :fire: Remove unused code from Screen component

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -12,39 +12,20 @@ const Wrapper = styled.div`
   margin: 2rem 0;
   padding: 0 2rem;
   box-sizing: border-box;
-  border: 2px solid white;
 
   // left sidebar
   > *:nth-child(1) {
-    border: 2px solid red;
     flex-basis: 200px;
   }
 
   // main content, between sidebars
   > *:nth-child(2) {
-    border: 2px solid blue;
   }
 
   // right sidebar
   > *:nth-child(3) {
-    border: 2px solid green;
     flex-basis: 200px;
   }
-`;
-
-const Screen1 = styled.div`
-  height: 100%;
-  width: 100%;
-  margin: 0 ${({ theme }) => theme.size.gap};
-  // border: ${({ theme }) => theme.size.border} solid ${({ theme }) => theme.color.elements.line};
-  border-radius: ${({ theme }) => theme.size.borderRadius}
-  box-sizing: border-box;
-  background: ${({ theme }) => theme.color.background.base}
-`;
-
-const Sidebar = styled.div`
-  width: 200px;
-  height: 100%;
 `;
 
 const Screen = ({ children, ...rest }) => (
